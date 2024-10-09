7cf36dd11d9a29eff81d23bf9f3599469963878d
Mahipal <mahipal@codzgarage.com>
2024-10-09 18:00:22 +0530

    coadble

---
+++ Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/21_Codable.swift
@@ -0,0 +1,18 @@
+//
+//  21_Codable.swift
+//  ContinuedLearning
+//
+//  Created by Codzgarage on 10/9/24.
+//
+
+import SwiftUI
+
+struct CodableDemo: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CodableDemo()
+}
