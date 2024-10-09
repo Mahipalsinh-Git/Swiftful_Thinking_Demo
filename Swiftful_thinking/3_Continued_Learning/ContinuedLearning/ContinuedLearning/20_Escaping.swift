16f0f76722905349e85bef092b3ddbe5f58595ba
Mahipal <=>
2024-10-09 07:27:50 +0530

    escaping demo

---
+++ Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/20_Escaping.swift
@@ -0,0 +1,18 @@
+//
+//  20_Escaping.swift
+//  ContinuedLearning
+//
+//  Created by Mahipal on 09/10/24.
+//
+
+import SwiftUI
+
+struct _0_Escaping: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _0_Escaping()
+}
