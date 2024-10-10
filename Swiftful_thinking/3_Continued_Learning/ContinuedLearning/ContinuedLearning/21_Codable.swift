81fb5b89558a7b4260e9ea1cce1882c9e9e1c2ee
Mahipal <=>
2024-10-10 07:40:53 +0530

    working on codablw

--- Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/21_Codable.swift
+++ Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/21_Codable.swift
@@ -7,7 +7,21 @@
 
 import SwiftUI
 
+struct CustomerModel: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+    let points: Int
+    let isPremium: Bool
+}
+
+class CodableViewModel: ObservableObject {
+    
+}
+
 struct CodableDemo: View {
+    
+    @StateObject var vm = CodableViewModel()
+    
     var body: some View {
         Text("Hello, World!")
     }
