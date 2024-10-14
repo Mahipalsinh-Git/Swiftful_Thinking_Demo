7637489f36d3916be1b19f2196892ead36644955
Mahipal <=>
2024-10-14 08:06:17 +0530

    backup data

--- Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/21_Codable.swift
+++ Swiftful_thinking/3_Continued_Learning/ContinuedLearning/ContinuedLearning/21_Codable.swift
@@ -7,15 +7,82 @@
 
 import SwiftUI
 
-struct CustomerModel: Identifiable {
-    let id = UUID().uuidString
+struct CustomerModel: Identifiable, Decodable {
+    let id: String
     let name: String
     let points: Int
     let isPremium: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case points
+        case isPremium
+    }
+    
+    init(id: String, name: String, points: Int, isPremium: Bool) {
+        self.id = id
+        self.name = name
+        self.points = points
+        self.isPremium = isPremium
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.points = try container.decode(Int.self, forKey: .points)
+        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
+    }
 }
 
 class CodableViewModel: ObservableObject {
     
+    @Published var customer: CustomerModel?
+    
+    init () {
+        getData()
+    }
+    
+    func getData() {
+        guard let data = getJsonData() else { return }
+        
+        // Way - 1
+        //        if
+        //            let localData = try? JSONSerialization.jsonObject(with: data),
+        //            let dict = localData as? [String: Any],
+        //            let id = dict["id"] as? String,
+        //            let name = dict["name"] as? String,
+        //            let points = dict["points"] as? Int,
+        //            let isPremium = dict["isPremium"] as? Bool {
+        //
+        //            customer = CustomerModel(id: id, name: name, points: points, isPremium: isPremium)
+        //        }
+        
+        // Way - 2
+        do {
+            self.customer = try JSONDecoder().decode(CustomerModel.self, from: data)
+        } catch let error {
+            print("Error in Decoding: \(error.localizedDescription)")
+        }
+    }
+    
+    func getJsonData() -> Data? {
+        
+        let dict: [String: Any] = [
+            "id" : "12345",
+            "name" : "Mahipal",
+            "points" : 100,
+            "isPremium" : true
+        ]
+        
+        let jsonData = try? JSONSerialization.data(
+            withJSONObject: dict,
+            options: .prettyPrinted
+        )
+        
+        return jsonData;
+    }
 }
 
 struct CodableDemo: View {
@@ -23,7 +90,14 @@ struct CodableDemo: View {
     @StateObject var vm = CodableViewModel()
     
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if let customerObj = vm.customer {
+                Text(customerObj.id)
+                Text(customerObj.name)
+                Text("\(customerObj.points)")
+                Text("is Premium = \(customerObj.isPremium)")
+            }
+        }
     }
 }
 
